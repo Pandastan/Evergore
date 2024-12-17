7a48f4c5fa6636ae1ebfdf3b6b0764abb3c28580
Alex <alexander.v.schmalz@gmail.com>
2024-12-17 11:43:38 +0100

    Update EG_Kampfauswerter_simple.user.js

--- scripts/EG_Kampfauswerter_simple.user.js
+++ scripts/EG_Kampfauswerter_simple.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Simpler Kampfauswerter 2
 // @namespace    http://tampermonkey.net/
-// @downloadURL  https://github.com/Pandastan/Evergore/blob/master/scripts/EG_Kampfauswerter_simple.user.js
-// @updateURL    https://github.com/Pandastan/Evergore/blob/master/scripts/EG_Kampfauswerter_simple.user.js
+// @downloadURL  https://github.com/Pandastan/Evergore/raw/refs/heads/master/scripts/EG_Kampfauswerter_simple.user.js
+// @updateURL    https://github.com/Pandastan/Evergore/raw/refs/heads/master/scripts/EG_Kampfauswerter_simple.user.js
 // @version      1.0
 // @description  Berechnet Werte aus Kampfereignisse logs.
 // @author       Eros
